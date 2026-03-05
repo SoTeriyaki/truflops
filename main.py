206e59b854e6d7a4f4ff4535d8c9a45e689efa02
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 23:24:21 +0100

    auto-commit

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     program_name = os.path.splitext(os.path.basename(dxf_path))[0]
 
     # tabela technologiczna
-    tech_table = get_tech_name(material, thickness)
+    tech_name = get_tech_name(material, thickness)
     tech_data = load_technology(tech_name)
 
     # parsowanie DXF
