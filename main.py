81af5bb3510c96a249b142b1e8e866addc26f82d
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 10:38:28 +0100

    auto-commit

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
-from datetime import datetime
+from config.technology import get_tech_table
+from post.writer import generate_program
+from dxf.parser import parse_dxf
 
-from config.technology import get_tech_table, load_tech_data
-from utils.file_utils import load_template
-from post.writer import apply_markers
+import os
+from datetime import datetime
 
 
 def main():
 
-    program_name = input("Nazwa programu: ")
-    material = input("Material (SC/SN/OC/AL): ")
+    dxf_path = input("Sciezka do DXF: ")
+    material = input("Material (SC/SN/OC/AL): ").upper()
     thickness = float(input("Grubosc blachy: "))
 
+    program_name = os.path.splitext(os.path.basename(dxf_path))[0]
+
+    # tabela technologiczna
     tech_table = get_tech_table(material, thickness)
-    tech_data = load_tech_data(tech_table)
 
-    template = load_template()
+    # parsowanie DXF
+    geometry = parse_dxf(dxf_path)
 
-    markers = {
+    print("Wczytana geometria:")
+    print(geometry)
 
-        "PROGRAM_NAME": program_name,
-        "DATE": datetime.now().strftime("%d.%m.%Y"),
-        "MATERIAL": material,
-        "MATERIAL_ISO": "1.0038",
-        "THICKNESS": thickness,
-        "TECH_TABLE": tech_table,
-        "TECH_DATA": tech_data,
-        "SHEET_NAME": f"ST{int(thickness*1000):06}----3000x1500",
+    # data
+    date = datetime.now().strftime("%d.%m.%Y")
 
-        "TOOLPATH": "",
-        "PART_PLACEMENTS": "",
-        "PART_POSITIONS": "",
-
-        "PROGRAM_PATH": "",
-        "HTML_PATH": ""
-    }
-
-    program = apply_markers(template, markers)
-
-    with open("output/generated.lst", "w", encoding="utf-8") as f:
-        f.write(program)
-
-    print("Program wygenerowany.")
+    # generowanie programu
+    generate_program(
+        program_name,
+        thickness,
+        tech_table,
+        date,
+        geometry
+    )
 
 
 if __name__ == "__main__":
