fbcc99c0238766f0d494cd816fc868018ab121a4
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 22:13:12 +0100

    auto-commit

--- config/technology_selector.py
+++ config/technology_selector.py
@@ -1,8 +1,14 @@
 TECH_TABLE = {
 
-    ("SC",4): "ST004MD0-O2SO-30-2",
+    # OCYNK
+    ("OC",2): "SS020MD0-N2SO-30-2",
+
+    # NIERDZEWKA
+    ("SN",1): "ST004MD0-O2SO-30-2",
     ("SC",6): "ST006MD0-O2SO-30-2",
 
+
+    # CZARNA
     ("SN",1.5): "SS015MD0-N2SO-30-2",
     ("SN",2): "SS020MD0-N2SO-30-2"
 
