c3e498cb1305a4ac7fde64bc67a597e818caab7f
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 12:29:43 +0100

    auto-commit

--- nest/placement.py
+++ nest/placement.py
@@ -9,11 +9,15 @@ def generate_part_placements(program_name, bbox, quantity):
 
     i = 1
 
+    part_w = bbox["width"]
+    part_h = bbox["height"]
+
     for p in placements:
 
         line = (
             f"DA,{i},'{program_name}','LO',"
-            f"{p['x']:.3f},{p['y']:.3f},0,0,0"
+            f"{p['x']:.3f},{p['y']:.3f},"
+            f"{part_w:.3f},{part_h:.3f},0"
         )
 
         lines.append(line)
