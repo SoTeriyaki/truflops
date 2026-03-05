cffc9c0e040dc11e254fae39dcdae7951c829970
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 10:28:52 +0100

    auto-commit

---
+++ config/technology.py
@@ -0,0 +1,36 @@
+TECH_TABLES = {
+
+    "SC": {  # stal czarna
+        0.5: "sc05",
+        1: "sc1",
+        1.5: "sc15",
+        2: "sc2",
+        2.5: "sc25",
+        3: "sc3",
+        4: "sc4",
+        5: "sc5",
+        6: "sc6",
+        8: "sc8",
+        10: "sc10",
+    },
+
+    "SN": {  # nierdzewna
+        1: "sn1",
+        2: "sn2",
+        3: "sn3",
+        4: "sn4",
+    },
+
+    "OC": {  # ocynk
+        1: "oc1",
+        2: "oc2",
+        3: "oc3",
+    },
+
+    "AL": {  # aluminium
+        1: "al1",
+        2: "al2",
+        3: "al3",
+    }
+
+}
