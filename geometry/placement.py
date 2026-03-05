1a61a7c336311c9b2bf79b85f0245f3a86d0b5f3
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 12:00:18 +0100

    auto-commit

--- geometry/placement.py
+++ geometry/placement.py
@@ -2,7 +2,7 @@ SAFE_OFFSET_X = 0
 SAFE_OFFSET_Y = 10
 
 
-def generate_part_placements(program_name, bbox):
+def generate_part_placements(program_name, bbox, quantity)
 
     x = SAFE_OFFSET_X
     y = SAFE_OFFSET_Y
