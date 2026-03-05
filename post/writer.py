3505ed693600672e74c31f70360b95dcb854e6db
SoTeriyaki <chillwagonchillwagon@icloud.com>
2026-03-05 10:41:40 +0100

    auto-commit

--- post/writer.py
+++ post/writer.py
@@ -1,3 +1,6 @@
+import os
+
+
 def apply_markers(template, markers):
 
     for key, value in markers.items():
@@ -8,3 +11,36 @@ def apply_markers(template, markers):
         )
 
     return template
+
+
+def generate_program(program_name, thickness, tech_table, date, geometry):
+
+    template_path = "templates/template.lst"
+    output_path = "output/generated.lst"
+
+    # wczytanie template
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = f.read()
+
+    # markery
+    markers = {
+        "PROGRAM_NAME": program_name,
+        "THICKNESS": thickness,
+        "DATE": date,
+        "TECH_TABLE": tech_table,
+        "TOOLPATH": "",
+        "PART_PLACEMENTS": ""
+    }
+
+    # podmiana markerów
+    program = apply_markers(template, markers)
+
+    # stworzenie folderu output jeśli nie istnieje
+    os.makedirs("output", exist_ok=True)
+
+    # zapis pliku
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(program)
+
+    print("Program wygenerowany:")
+    print(output_path)
